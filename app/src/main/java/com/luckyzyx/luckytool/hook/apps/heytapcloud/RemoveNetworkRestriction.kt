eb19e63e64368c689d5ec57c63d9a19be32349bb
luckyzyx <2813467705@qq.com>
2022-11-15 19:46:34 +0800

    Adaptation Cloud service mobile network limitation

--- app/src/main/java/com/luckyzyx/luckytool/hook/apps/heytapcloud/RemoveNetworkRestriction.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/apps/heytapcloud/RemoveNetworkRestriction.kt
@@ -8,15 +8,15 @@ import com.highcapable.yukihookapi.hook.type.java.IntType
 class RemoveNetworkRestriction : YukiBaseHooker() {
     override fun onHook() {
         //Source NetworkUtil
-        //Search -> MOBILE -> method
+        //Search -> Const.Callback.NetworkState.NetworkType.NETWORK_MOBILE -> Method
         searchClass {
             from("com.cloud.base.commonsdk.baseutils", "t2")
-            constructor().count(0)
-            field().count(0)
-            method().count(14)
+            constructor().none()
+            field().none()
+            method().count(13..14)
             method {
                 param(ContextClass)
-            }.count(7)
+            }.count(6..7)
             method {
                 param(IntType)
             }.count(5)
