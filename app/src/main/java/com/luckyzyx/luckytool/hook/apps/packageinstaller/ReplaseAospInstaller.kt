4869646351af18e30a3b462a3f5ea0d52dbe43c1
luckyzyx <2813467705@qq.com>
2022-11-20 23:51:48 +0800

    [修复] AOSP安装器调用问题

--- app/src/main/java/com/luckyzyx/luckytool/hook/apps/packageinstaller/ReplaseAospInstaller.kt
+++ app/src/main/java/com/luckyzyx/luckytool/hook/apps/packageinstaller/ReplaseAospInstaller.kt
@@ -1,31 +1,40 @@
 package com.luckyzyx.luckytool.hook.apps.packageinstaller
 
-import com.highcapable.yukihookapi.hook.bean.VariousClass
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.field
-import com.highcapable.yukihookapi.hook.type.android.BundleClass
-import com.highcapable.yukihookapi.hook.type.java.BooleanType
 
 class ReplaseAospInstaller : YukiBaseHooker() {
     override fun onHook() {
         //search class -> DeleteStagedFileOnResult
         //search class.method -> onCreate +4 -> class.method
-        findClass("com.android.packageinstaller.DeleteStagedFileOnResult").hook{
+//        findClass("com.android.packageinstaller.DeleteStagedFileOnResult").hook {
+//            injectMember {
+//                method {
+//                    name = "onCreate"
+//                    param(BundleClass)
+//                }
+//                beforeHook {
+//                    args(0).setNull()
+//                    VariousClass(
+//                        "com.android.packageinstaller.oplus.common.j",
+//                        "com.android.packageinstaller.oplus.common.FeatureOption"
+//                    ).toClass().field {
+//                        type(BooleanType).index(1)
+//                    }.get().setTrue()
+//                }
+//            }
+//        }
+
+        //Source FeatureOption
+        findClass("com.android.packageinstaller.oplus.common.FeatureOption").hook {
             injectMember {
                 method {
-                    name = "onCreate"
-                    param(BundleClass)
+                    name = "setIsClosedSuperFirewall"
+                    paramCount = 1
                 }
-                beforeHook {
-                    VariousClass(
-                        "com.android.packageinstaller.oplus.common.j",
-                        "com.android.packageinstaller.oplus.common.FeatureOption"
-                    ).toClass().field {
-                        type(BooleanType).index(1)
-                    }.get().setTrue()
+                afterHook {
+                    field { name = "sIsClosedSuperFirewall" }.get().setTrue()
                 }
             }
         }
-
     }
 }
